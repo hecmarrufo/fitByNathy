66929f5584d69e5b86871ca3e80717733db96070
Hector Marrufo <marrufofinol@gmail.com>
2024-01-25 18:23:59 -0300

    fix build

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import Home from "./pages/spa/home";
-import About from "./pages/about/About";
+// import About from "./pages/about/About";
 import Contact from "./pages/contact/Contact";
 import Gallery from "./pages/gallery/Gallery";
 import NotFound from "./pages/notFound/NotFound";
